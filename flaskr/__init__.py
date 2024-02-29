e3099fda914e8054af3066802c150323a063c144
Shoki Matsumoto <152942447+SK-Mat@users.noreply.github.com>
2024-03-01 04:40:04 +0900

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 from datetime import timedelta
 app = Flask(__name__)
-app.config['PERMANENT_SESSION_LIFETIME'] = timedalta(minutes=3)#session time（10分）
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=3)#session time（10分）
 import flaskr.main
